63d6782a10d602b2275e94366fd37d8aecb825ea
winhubsolutions <57292342+winhubsolutions@users.noreply.github.com>
2022-02-09 15:34:40 +0530

    Update JobForm.js

--- src/components/Contact/JobForm.js
+++ src/components/Contact/JobForm.js
@@ -10,7 +10,7 @@ export default function ContactForm() {
         <div>
        
 
-<iframe src='https://businesso.vigilant-burnell.147-182-171-252.plesk.page/job' frameBorder='0'  width='100%' height='2300' allowtransparency='true'/>
+<iframe src='https://cadvision.xyz/job' frameBorder='0'  width='100%' height='2300' allowtransparency='true'/>
 </div>
 </>
 
